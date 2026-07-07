8c230f1add3791e0df01b25ab604087f7c80efb8
Marco Meyer-Conde <marco@tcu.ac.jp>
2026-07-07 13:23:45 +0900

    Fill in real source, license, and build steps for Ninja 1.2.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,14 @@
 #! /usr/bin/bash
 set -e
 
-mkdir -p build
-cd build
+cp "${BUILD_PREFIX}/share/gnuconfig/config.sub" "${BUILD_PREFIX}/share/gnuconfig/config.guess" .
+autoreconf --install
 
-cmake .. \
-  -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-  -DCMAKE_BUILD_TYPE=Release
+./configure --prefix="${PREFIX}" --enable-static=no \
+  --with-avholo="${FFLAGS} -lavh_olo" \
+  --with-looptools="${FLDFLAGS} -looptools -lgfortran -lquadmath" \
+  FCINCLUDE="${FCINCLUDE} -I${PREFIX}/include/oneloop"
 
 NPROC=$(nproc 2>/dev/null || sysctl -n hw.ncpu)
-make -j"$NPROC"
+make -j"${NPROC}"
 make install
